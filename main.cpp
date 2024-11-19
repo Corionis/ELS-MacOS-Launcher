ff9d8490fb9c407f075b6c8101e696cffca025e1
Corionis <rockney8588@gmail.com>
2024-11-19 15:42:56 -0700

    For ELS #167 Added check for default ELS directory; fixed logging

--- main.cpp
+++ main.cpp
@@ -26,14 +26,26 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (isLogging)
-        log.open("/Users/trh/Work/corionis/ELS/mock/output/ELS-MacOS-Launcher.log");
+    // get user home directory
+    char *homeChar = getenv("HOME");
+    std::string homeStr = homeChar;
 
+    if (isLogging)
+    {
+        std::string homeLog = homeStr + "/.els/output";
+
+        homeLog += "/ELS-MacOS-Launcher.log";
+        log.open(homeLog);
+        log << "log: " << homeLog << std::endl;
+    }
+
+    // get the current working directory
     getcwd(cwd, sizeof(cwd));
     if (isLogging)
         log << "cwd! " << cwd << std::endl;
 
-    // find the root from argv[0] by removing /ELS-Navigator.app/Contents/MacOS/ELS-Navigator
+    // get the path to this executable
+    // from the root of argv[0] by removing /ELS-Navigator.app/Contents/MacOS/ELS-Navigator
     if (argc > 0)
     {
         if (isLogging)
@@ -91,10 +103,10 @@ int main(int argc, char *argv[]) {
             }
 
             // make sure the directory exists
-            std::filesystem::path directoryPath(cwd);
-            if (std::filesystem::exists(directoryPath))
+            std::filesystem::path cfgPath(cwd);
+            if (std::filesystem::exists(cfgPath))
             {
-                if (!std::filesystem::is_directory(directoryPath))
+                if (!std::filesystem::is_directory(cfgPath))
                 {
                     if (isLogging)
                         log << "Exception: -C \"" << cwd << "\" exists but is not a directory" << std::endl;
@@ -103,7 +115,7 @@ int main(int argc, char *argv[]) {
             }
             else
             {
-                mkdir(cwd, 0x755);
+                mkdir(cwd, 0755);
                 chdir(cwd);
             }
             break;
@@ -124,8 +136,8 @@ int main(int argc, char *argv[]) {
 
     // setup arguments
     int sz = dashC ? argc + 5 : argc + 3;
-    if (isLogging)
-        sz = sz - 1; // --launcher-log is for the launcher only and skipped for the Navigator
+    if (isLogging) // --launcher-log is for the launcher only and skipped for the Navigator
+        sz = sz - 1;
     char *arguments[sz];
     if (isLogging)
         log << "siz: " << sz << std::endl;
@@ -146,9 +158,52 @@ int main(int argc, char *argv[]) {
     arg3 += "/bin/ELS.jar";
     arguments[index++] = arg3.data();
 
-    // add -C [working-directory] if not specified
+    // add -C [working-directory] if needed
     if (dashC)
     {
+        // see if libraries directory exists in the software location
+        char check[PATH_MAX];
+        std::string sd = cwd;
+        sd += "/libraries";
+        strncpy(check, &sd[0], sd.length());
+
+        // make sure the directory exists
+        std::filesystem::path programPath(check);
+        if (std::filesystem::exists(programPath)) // check if libraries is in program directory
+        {
+            if (!std::filesystem::is_directory(programPath))
+            {
+                std::cout << "Exception: \"" << sd << "\" exists but is not a directory" << std::endl;
+                return 1;
+            }
+        }
+        else // not in program directory, use default of "USER_HOME/.els"
+        {
+            char defaultDirectory[PATH_MAX];
+            std::string dd = homeChar;
+
+            std::cout << "hom: " << homeStr << std::endl;
+            dd += "/.els";
+            strncpy(defaultDirectory, &dd[0], dd.length());
+
+            // make sure the directory exists
+            std::filesystem::path defaultPath(defaultDirectory);
+            if (std::filesystem::exists(defaultPath))
+            {
+                if (!std::filesystem::is_directory(defaultPath))
+                {
+                    std::cout << "Exception: \"" << dd << "\" exists but is not a directory" << std::endl;
+                    return 1;
+                }
+            }
+            else
+                mkdir(defaultDirectory, 0775); // create default directory
+
+            for (int i = 0; i < sizeof(cwd); ++i)
+                cwd[i] = '\0';
+            strcpy(cwd, defaultDirectory);
+        }
+
         std::string arg = "-C";
         arguments[index++] = arg.data();
         arguments[index++] = cwd;
@@ -158,7 +213,7 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i)
     {
         std::string search(argv[i]);
-        int p = search.find("--launcher-log");
+        int p = search.find("--launcher-log"); // exclude launcher option
         if (p >= 0)
             continue;
         arguments[index++] = argv[i];
@@ -176,8 +231,7 @@ int main(int argc, char *argv[]) {
 
     if (isLogging)
     {
-        log << "complete: " << command << " ";
-        log << complete << std::endl;
+        log << "complete: " << command << " " << complete << std::endl;
     }
 
     // run it
